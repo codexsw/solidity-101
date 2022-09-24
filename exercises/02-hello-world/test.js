4a1387361ac2bd314bf8dd80bc8f0b1aed30c01d
Ernesto Freyre <ernestofreyreg@gmail.com>
2022-09-24 11:44:09 -0700

    spawn

--- exercises/02-hello-world/test.js
+++ exercises/02-hello-world/test.js
@@ -1,14 +1,25 @@
-const { onExit } = require("@rauschma/stringio");
 const { spawn } = require("child_process");
 
 async function main() {
-  const childProcess = spawn("npx", ["hardhat", "test", "test/02-test.ts"], {
-    stdio: [process.stdin, process.stdout, process.stderr],
+  return new Promise((resolve, reject) => {
+    const ls = spawn("npx", ["hardhat", "test", "test/02-test.ts"], {
+      stdio: [process.stdin, process.stdout, process.stderr],
+    });
+
+    ls.stdout.on("data", function (data) {
+      console.log("stdout: " + data.toString());
+    });
+
+    ls.stderr.on("data", function (data) {
+      console.log("stderr: " + data.toString());
+    });
+
+    ls.on("exit", function (code) {
+      console.log("child process exited with code " + code.toString());
+    });
+
+    console.log("### DONE");
   });
-
-  await onExit(childProcess);
-
-  console.log("### DONE");
 }
 
 test("02-steps", async () => {
