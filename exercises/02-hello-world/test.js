9636417fa782665ad283302fe80b024c33e0794b
Ernesto Freyre <ernestofreyreg@gmail.com>
2022-09-24 14:39:29 -0700

    tests

--- exercises/02-hello-world/test.js
+++ exercises/02-hello-world/test.js
@@ -1,5 +1,5 @@
 const { npx } = require("../../scripts/helper");
 
 test("02-steps", async () => {
-  expect(await npx(["hardhat", "test", "test/02-test.ts"])).toBe(0);
+  expect(await npx(["hardhat", "test", "test/02.test.ts"])).toBe(0);
 });
