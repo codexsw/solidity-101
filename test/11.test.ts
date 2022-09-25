ae0500f03bbaeea54a4bc1a9c32fc93bdb88b470
Ernesto Freyre <ernestofreyreg@gmail.com>
2022-09-25 04:52:31 +0000

    11

--- test/11.test.ts
+++ test/11.test.ts
@@ -13,10 +13,55 @@ describe("11", function () {
   describe("deposit", function () {
     it("deposit 1 ether", async function () {
       const { bank, addr1 } = await loadFixture(deployOneYearLockFixture);
-      await bank.deposit({ value: ethers.utils.parseEther("1") });
-      expect(await bank.getBalance(addr1.address)).to.equal(
+
+      await expect(
+        bank.connect(addr1).deposit({ value: ethers.utils.parseEther("1") })
+      ).to.changeEtherBalance(addr1, ethers.utils.parseEther("-1.0"));
+
+      expect(await bank.connect(addr1).getBalance()).to.equal(
         ethers.utils.parseEther("1")
       );
+    });
+
+    it("more deposits", async function () {
+      const { bank, addr1, addr2 } = await loadFixture(
+        deployOneYearLockFixture
+      );
+
+      await expect(
+        bank.connect(addr1).deposit({ value: ethers.utils.parseEther("0.5") })
+      ).to.changeEtherBalance(addr1, ethers.utils.parseEther("-0.5"));
+
+      await await expect(() =>
+        bank.connect(addr2).deposit({ value: ethers.utils.parseEther("0.25") })
+      ).to.changeEtherBalance(addr2, ethers.utils.parseEther("-0.25"));
+
+      expect(await ethers.provider.getBalance(bank.address)).to.equal(
+        ethers.utils.parseEther("0.75")
+      );
+    });
+  });
+
+  describe("withdraw", function () {
+    it("widthdraw 1 ether", async function () {
+      const { bank, addr1 } = await loadFixture(deployOneYearLockFixture);
+
+      await expect(
+        bank.connect(addr1).deposit({ value: ethers.utils.parseEther("2") })
+      ).to.changeEtherBalance(addr1, ethers.utils.parseEther("-2.0"));
+
+      await expect(
+        bank.connect(addr1).widthdraw(ethers.utils.parseEther("1.0"))
+      ).to.changeEtherBalance(addr1, ethers.utils.parseEther("1.0"));
+    });
+
+    it("reverted if lower balance", async function () {
+      const { bank, addr1 } = await loadFixture(deployOneYearLockFixture);
+
+      bank.connect(addr1).deposit({ value: ethers.utils.parseEther("2.0") });
+      await expect(
+        bank.connect(addr1).widthdraw(ethers.utils.parseEther("3.0"))
+      ).to.be.revertedWith("low-balance");
     });
   });
 });
