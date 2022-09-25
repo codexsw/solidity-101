c2b94e42ede6d555ff8d6be9da16012e97d315df
Ernesto Freyre <ernestofreyreg@gmail.com>
2022-09-25 07:33:01 +0000

    12

--- test/12.test.ts
+++ test/12.test.ts
@@ -5,19 +5,15 @@ import { ethers } from "hardhat";
 describe("12", function () {
   async function deployOneYearLockFixture() {
     const EtherBank = await ethers.getContractFactory("EtherBank");
-    const Attack = await ethers.getContractFactory("Attack");
     const bank = await EtherBank.deploy();
-    const attacker = await Attack.deploy(bank.address);
 
     const [owner, addr1, addr2] = await ethers.getSigners();
-    return { bank, owner, addr1, addr2, attacker };
+    return { bank, owner, addr1, addr2 };
   }
 
   describe("deposit", function () {
     it("widthdraw with re-entrancy guard", async function () {
-      const { bank, addr1, attacker, addr2 } = await loadFixture(
-        deployOneYearLockFixture
-      );
+      const { bank, addr1 } = await loadFixture(deployOneYearLockFixture);
 
       await expect(
         bank.connect(addr1).deposit({ value: ethers.utils.parseEther("10") })
