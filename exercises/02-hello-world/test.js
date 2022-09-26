80cc9d3c6a986ba9438b6354bb050ca40acbd8c9
Ernesto Freyre <ernestofreyreg@gmail.com>
2022-09-25 21:04:38 -0700

    02

--- exercises/02-hello-world/test.js
+++ exercises/02-hello-world/test.js
@@ -1,4 +1,20 @@
 const { npx } = require("../../scripts/helper");
+const fs = require("fs");
+const { expect } = require("chai");
+
+const fileExists = (path) => {
+  try {
+    if (fs.existsSync(path)) {
+      return true;
+    }
+  } catch (err) {
+    return false;
+  }
+};
+
+test("contract-exists", async () => {
+  expect(fileExists("../../contracts/HelloWorld.sol")).to.be.true;
+});
 
 test("02-steps", async () => {
   expect(await npx(["hardhat", "test", "test/02.test.ts"])).toBe(0);
